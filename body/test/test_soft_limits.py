eb3163750a3f4dc2381b395f729087a5862737cf
Aaron Edsinger <aedsinger@hello-robot.com>
2021-05-27 19:58:53 -0700

    added unit tests for soft limits

--- body/test/test_soft_limits.py
+++ body/test/test_soft_limits.py
@@ -1,6 +1,6 @@
 import unittest
 import stretch_body.arm
-
+import time
 
 class TestArmSoftLimitHoming(unittest.TestCase):
 
@@ -18,3 +18,21 @@ class TestArmSoftLimitHoming(unittest.TestCase):
         a.stop()
 
 
+    def test_move_arm_with_soft_limits(self):
+        """
+        Ensure that soft limits actual clip range of motion
+        """
+        a = stretch_body.arm.Arm()
+        a.motor.disable_sync_mode()
+        self.assertTrue(a.startup())
+        a.pull_status()
+        self.assertTrue(a.motor.status['pos_calibrated'])
+        limit_pos = 0.2
+        a.set_soft_motion_limits(0,limit_pos)
+        a.push_command()
+        a.move_to(x_m=0.3)
+        a.push_command()
+        time.sleep(3.0)
+        a.pull_status()
+        self.assertAlmostEqual(a.status['pos'], limit_pos, places=1)
+        a.stop()
