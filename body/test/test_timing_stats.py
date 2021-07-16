6b4200ee5cccf3e31b6fa17450c284dd096bd5b1
hello-binit <64861565+hello-binit@users.noreply.github.com>
2021-07-16 18:17:15 -0400

    Measure isolated thread performance

--- body/test/test_timing_stats.py
+++ body/test/test_timing_stats.py
@@ -28,8 +28,8 @@ class TestTimingStats(unittest.TestCase):
         # s.display_rate_histogram()
         self.assertTrue(s.status['loop_warns'] == 0)
 
-    @unittest.skip(reason='Currently hittin ~11hz for dxl')
-    def test_robot_loops(self):
+    @unittest.skip(reason='Currently hitting ~11hz for dxl')
+    def test_robot_loops(self, rate_missed_threshold=0.1):
         print('Starting test_robot_loops')
         r = robot.Robot()
         r.startup()
@@ -37,20 +37,44 @@ class TestTimingStats(unittest.TestCase):
         r.non_dxl_thread.stats.pretty_print()
         r.dxl_thread.stats.pretty_print()
         r.stop()
-        self.assertTrue(r.dxl_thread.stats.status['loop_warns'] == 0)
-        self.assertTrue(r.non_dxl_thread.stats.status['loop_warns'] == 0)
+        self.assertLess(r.dxl_thread.stats.status['loop_warns'] / r.dxl_thread.stats.loop_cycles, rate_missed_threshold)
+        self.assertLess(r.non_dxl_thread.stats.status['loop_warns'] / r.non_dxl_thread.stats.loop_cycles, rate_missed_threshold)
 
     @unittest.skip(reason='Currently hitting ~6hz and ~4hz for non_dxl and dxl respectively')
-    def test_robot_loops_heavy(self):
+    def test_robot_loops_heavy(self, rate_missed_threshold=0.1):
         print('Starting test_robot_loops')
         r = robot.Robot()
         r.startup()
         start = time.time()
-        while time.time() - start < 3.0:
+        while time.time() - start < 20.0:
             x = np.random.rand(3, 1000, 1000)
             x.tolist()
         r.non_dxl_thread.stats.pretty_print()
         r.dxl_thread.stats.pretty_print()
         r.stop()
-        self.assertTrue(r.dxl_thread.stats.status['loop_warns'] == 0)
-        self.assertTrue(r.non_dxl_thread.stats.status['loop_warns'] == 0)
+        self.assertLess(r.dxl_thread.stats.status['loop_warns'] / r.dxl_thread.stats.loop_cycles, rate_missed_threshold)
+        self.assertLess(r.non_dxl_thread.stats.status['loop_warns'] / r.non_dxl_thread.stats.loop_cycles, rate_missed_threshold)
+
+    @unittest.skip(reason='Used to measure isolated perf, doesnt exit')
+    def test_dxl_on_main_thread(self):
+        r = robot.Robot()
+        for k in r.devices.keys():
+            if r.devices[k] is not None:
+                if not r.devices[k].startup():
+                    r.logger.warning('device %s failed to start' % k)
+        target = 11.0
+        dxl_thread = robot.DXLStatusThread(r, target_rate_hz=target)
+        dxl_thread.run()
+        self.assertAlmostEqual(dxl_thread.stats.status['loop_rate_av=g_hz'], target)
+
+    @unittest.skip(reason='Used to measure isolated perf, doesnt exit')
+    def test_non_dxl_on_main_thread(self):
+        r = robot.Robot()
+        for k in r.devices.keys():
+            if r.devices[k] is not None:
+                if not r.devices[k].startup():
+                    r.logger.warning('device %s failed to start' % k)
+        target = 25.0
+        non_dxl_thread = robot.NonDXLStatusThread(r, target_rate_hz=target)
+        non_dxl_thread.run()
+        self.assertAlmostEqual(non_dxl_thread.stats.status['loop_rate_av=g_hz'], target)
