a87c29325d1a0492fcccb0e60cf376f003e34ffa
hello-binit <64861565+hello-binit@users.noreply.github.com>
2021-08-05 03:03:14 -0400

    Improved waypoint traj unit test with interpolation

--- body/test/test_steppers.py
+++ body/test/test_steppers.py
@@ -5,6 +5,7 @@ stretch_body.robot_params.RobotParams.set_logging_level("DEBUG")
 import unittest
 import stretch_body.stepper
 import stretch_body.pimu
+from stretch_body.hello_utils import evaluate_polynomial_at
 
 import time
 
@@ -98,8 +99,6 @@ class TestSteppers(unittest.TestCase):
 
         Spline coefficients calculated in Desmos at:
         https://www.desmos.com/calculator/atv5ilhodq
-
-        TODO: Check plotted spline fits ideal spline within certain error
         """
         s = stretch_body.stepper.Stepper('/dev/hello-motor-lift')
         self.assertTrue(s.startup())
@@ -123,11 +122,13 @@ class TestSteppers(unittest.TestCase):
                       i_contact_pos=i_contact_pos,
                       i_contact_neg=i_contact_neg)
         s.push_command()
-        time.sleep(7)
+        s.wait_until_at_setpoint()
         s.pull_status()
-        self.assertAlmostEqual(s.status['pos'], position_rad, places=2)
+        self.assertAlmostEqual(s.status['pos'], position_rad, places=1)
 
         # send waypoint trajectory
+        first_segment = [3.0, 62.425, 0, 0, -3.731, 1.866, -0.249, 2]
+        second_segment = [3.0, 52.35, 0, 0, 3.731, -1.866, 0.249, 3]
         s.enable_pos_traj_waypoint()
         # s.set_command(v_des=velocity_rad,
         #               a_des=acceleration_rad,
@@ -136,26 +137,35 @@ class TestSteppers(unittest.TestCase):
         #               i_contact_neg=i_contact_neg)
         s.push_command()
         time.sleep(0.1)
-        s.start_waypoint_trajectory([3.0, 62.425, 0, 0, -3.731, 1.866, -0.249, 2])
+        start_time = time.time()
+        s.start_waypoint_trajectory(first_segment)
         print(s.waypoint_traj_id)
+
+        # first segment executing
         for _ in range(10):
             # self.assertEqual(s.waypoint_traj_id, 2) # TODO: uC reports 1 unless next seg loaded
             print(s.waypoint_traj_id)
             time.sleep(0.1)
-            s.set_next_trajectory_segment([3.0, 52.35, 0, 0, 3.731, -1.866, 0.249, 3])
+            s.set_next_trajectory_segment(second_segment)
             s.pull_status()
-            self.assertLessEqual(s.status['pos'], 62.425)
-            self.assertGreaterEqual(s.status['pos'], 52.35)
-        time.sleep(2)
+            self.assertLessEqual(s.status['pos'], 62.425 + 1.0)
+            self.assertGreaterEqual(s.status['pos'], 52.35 - 1.0)
+            pos, _, _ = evaluate_polynomial_at(first_segment[1:-1], time.time() - start_time)
+            self.assertAlmostEqual(s.status['pos'], pos, places=-1)
+        time.sleep(2) # let the remainder of the first segment complete
+
+        # second segment executing
         for _ in range(10):
             # self.assertEqual(s.waypoint_traj_id, 3) # TODO: uC doesn't report 3
             print(s.waypoint_traj_id)
             time.sleep(0.1)
             s.pull_status()
-            self.assertLessEqual(s.status['pos'], 62.425)
-            self.assertGreaterEqual(s.status['pos'], 52.35)
-        time.sleep(2)
+            self.assertLessEqual(s.status['pos'], 62.425 + 1.0)
+            self.assertGreaterEqual(s.status['pos'], 52.35 - 1.0)
+            pos, _, _ = evaluate_polynomial_at(second_segment[1:-1], (time.time() - start_time) - 3.0)
+            self.assertAlmostEqual(s.status['pos'], pos, places=-1)
+        time.sleep(2) # let the remainder of the second segment complete
+
         s.pull_status()
         self.assertAlmostEqual(s.status['pos'], position_rad, places=1)
-
         s.stop()
