4962e0decffef4667fc966c7740e35e58b942658
hello-binit <64861565+hello-binit@users.noreply.github.com>
2021-08-05 21:01:28 -0400

    Added segment class and unit tests

--- body/stretch_body/trajectories.py
+++ body/stretch_body/trajectories.py
@@ -59,3 +59,63 @@ class Waypoint:
 
     def __ge__(self, other):
         return np.greater_equal(self.time, other.time)
+
+
+class Segment:
+
+    def __init__(self, segment_id, duration, a0, a1, a2, a3, a4, a5):
+        """A polynomial segment of a spline
+
+        Coefficients a0-a5 can represent up to a quintic polynomial as
+        f(t) = a0 + a1*t + a2*t^2 + a3*t^3 + a4*t^4 + a5*t^5
+
+        Attributes
+        ----------
+        segment_id : int
+            The required segment ID for pushing to the hardware
+        duration : float
+            The duration in seconds the polynomial is tracked
+        """
+        self.segment_id = segment_id
+        self.duration = duration
+        self.a0 = a0
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        self.a4 = a4
+        self.a5 = a5
+
+    def __repr__(self):
+        return "Segment(id={0}, duration={1}, a0={2}, a1={3}, a2={4}, a3={5}, a4={6}, a5={7})".format(
+            self.segment_id, self.duration, self.a0, self.a1, self.a2, self.a3, self.a4, self.a5)
+
+    def __eq__(self, other):
+        return np.isclose(self.duration, other.duration, atol=1e-2) and \
+               np.isclose(self.a0, other.a0, atol=1e-2) and \
+               np.isclose(self.a1, other.a1, atol=1e-2) and \
+               np.isclose(self.a2, other.a2, atol=1e-2) and \
+               np.isclose(self.a3, other.a3, atol=1e-2) and \
+               np.isclose(self.a4, other.a4, atol=1e-2) and \
+               np.isclose(self.a5, other.a5, atol=1e-2)
+
+    @classmethod
+    def zeros(cls, segment_id=2):
+        return cls(segment_id=segment_id, duration=0, a0=0, a1=0, a2=0, a3=0, a4=0, a5=0)
+
+    @classmethod
+    def from_array(cls, segment_arr, segment_id=None, duration=None):
+        if len(segment_arr) == 8:
+            segment_id = segment_arr.pop()
+        if len(segment_arr) == 7:
+            duration = segment_arr.pop(0)
+        if len(segment_arr) != 6:
+            raise ValueError("invalid number of elements in segment array")
+        return cls(segment_id, duration, *segment_arr)
+
+    def to_array(self, only_coeffs=False):
+        if only_coeffs:
+            return [self.a0, self.a1, self.a2, self.a3, self.a4, self.a5]
+        return [self.duration, self.a0, self.a1, self.a2, self.a3, self.a4, self.a5, self.segment_id]
+
+    def evaluate_at(self, t):
+        return hu.evaluate_polynomial_at(self.to_array(only_coeffs=True), t)
--- body/test/test_trajectories.py
+++ body/test/test_trajectories.py
@@ -67,3 +67,95 @@ class TestTrajectories(unittest.TestCase):
         self.assertFalse(w1 > w2)
         self.assertTrue(w1 <= w2)
         self.assertFalse(w1 >= w2)
+
+    def test_segment(self):
+        print('equal')
+        s1 = stretch_body.trajectories.Segment(segment_id=0, duration=0.0, a0=0.0, a1=0.0, a2=0.0, a3=0.0, a4=0.0, a5=0.0); print('s1: {0}'.format(s1))
+        s2 = stretch_body.trajectories.Segment(segment_id=1, duration=0.0, a0=0.0, a1=0.0, a2=0.0, a3=0.0, a4=0.0, a5=0.0); print('s2: {0}'.format(s2))
+        self.assertEqual(s1, s2)
+
+        print('not equal')
+        s1 = stretch_body.trajectories.Segment(segment_id=0, duration=0.0, a0=0.0, a1=0.0, a2=0.0, a3=0.0, a4=0.0, a5=0.0); print('s1: {0}'.format(s1))
+        s2 = stretch_body.trajectories.Segment(segment_id=1, duration=0.0, a0=0.0, a1=100.0, a2=-34.0, a3=0.0, a4=0.0, a5=0.0); print('s2: {0}'.format(s2))
+        self.assertNotEqual(s1, s2)
+
+        print('not equal')
+        segment_arr = [3.0, 62.425, 0, 0, -3.731, 1.866, -0.249]
+        s1 = stretch_body.trajectories.Segment.from_array(segment_arr, segment_id=0); print('s1: {0}'.format(s1))
+        s2 = stretch_body.trajectories.Segment(segment_id=1, duration=0.0, a0=0.0, a1=100.0, a2=-34.0, a3=0.0, a4=0.0, a5=0.0); print('s2: {0}'.format(s2))
+        self.assertNotEqual(s1, s2)
+
+        print('equal')
+        segment_arr = [3.0, 62.425, 0, 0, -3.731, 1.866, -0.249]
+        s1 = stretch_body.trajectories.Segment.from_array(segment_arr, segment_id=0); print('s1: {0}'.format(s1))
+        s2 = stretch_body.trajectories.Segment(segment_id=1, duration=3.0, a0=62.425, a1=0.0, a2=0.0, a3=-3.731, a4=1.866, a5=-0.249); print('s2: {0}'.format(s2))
+        self.assertEqual(s1, s2)
+
+        print('equal')
+        s1_arr = [3.0, 62.425, 0, 0, -3.731, 1.866, -0.249, 0]; print('s1: {0}'.format(s1_arr))
+        s2_arr = stretch_body.trajectories.Segment(segment_id=0, duration=3.0, a0=62.425, a1=0.0, a2=0.0, a3=-3.731, a4=1.866, a5=-0.249).to_array(); print('s2: {0}'.format(s2_arr))
+        self.assertEqual(s1_arr, s2_arr)
+
+        print('equal')
+        s1_arr = [62.425, 0, 0, -3.731, 1.866, -0.249]; print('s1: {0}'.format(s1_arr))
+        s2_arr = stretch_body.trajectories.Segment(segment_id=1, duration=3.0, a0=62.425, a1=0.0, a2=0.0, a3=-3.731, a4=1.866, a5=-0.249).to_array(only_coeffs=True); print('s2: {0}'.format(s2_arr))
+        self.assertEqual(s1_arr, s2_arr)
+
+        print('equal')
+        s1 = stretch_body.trajectories.Segment.zeros(); print('s1: {0}'.format(s1))
+        s2 = stretch_body.trajectories.Segment(segment_id=1, duration=0.0, a0=0.0, a1=0.0, a2=0.0, a3=0.0, a4=0.0, a5=0.0); print('s2: {0}'.format(s2))
+        self.assertEqual(s1, s2)
+
+    def test_from_invalid_segment_arr(self):
+        segment_arr = [4, 65]
+        with self.assertRaises(ValueError):
+            stretch_body.trajectories.Segment.from_array(segment_arr)
+
+        segment_arr = [4, 65, 4, 56, 36, 346, 345, 345, 346, 346, 234, 23]
+        with self.assertRaises(ValueError):
+            stretch_body.trajectories.Segment.from_array(segment_arr)
+
+    def test_segment_evaluate_at(self):
+        """Verify correctness of the Segment.evaluate_at method
+
+        Spline coefficients calculated in Desmos at:
+        https://www.desmos.com/calculator/atv5ilhodq
+        """
+        # Segment b
+        b1 = stretch_body.trajectories.Waypoint(time=0.0, position=62.425, velocity=0.0, acceleration=0.0)
+        b2 = stretch_body.trajectories.Waypoint(time=3.0, position=52.350, velocity=0.0, acceleration=0.0)
+        b  = stretch_body.trajectories.Segment(segment_id=2, duration=3.0, a0=62.425, a1=0, a2=0, a3=-3.7314815, a4=1.8657407, a5=-0.2487654)
+
+        pos, vel, accel = b.evaluate_at(b1.time - b1.time)
+        self.assertAlmostEqual(pos, b1.position, places=4)
+        self.assertAlmostEqual(vel, b1.velocity, places=4)
+        self.assertAlmostEqual(accel, b1.acceleration, places=4)
+
+        pos, vel, accel = b.evaluate_at(b2.time - b1.time)
+        self.assertAlmostEqual(pos, b2.position, places=4)
+        self.assertAlmostEqual(vel, b2.velocity, places=4)
+        self.assertAlmostEqual(accel, b2.acceleration, places=4)
+
+        pos, vel, accel = b.evaluate_at(1.3 - b1.time)
+        self.assertAlmostEqual(pos, 58.632, places=3)
+        self.assertNotAlmostEqual(vel, 0.0, places=4)
+        self.assertNotAlmostEqual(accel, 0, places=4)
+
+        # Segment c
+        c1 = stretch_body.trajectories.Waypoint(time=3.0, position=52.350, velocity=0.0, acceleration=0.0)
+        c2 = stretch_body.trajectories.Waypoint(time=6.0, position=62.425, velocity=0.0, acceleration=0.0)
+        c  = stretch_body.trajectories.Segment(segment_id=3, duration=3.0, a0=52.350, a1=0, a2=0, a3=3.7314815, a4=-1.8657407, a5=0.2487654)
+        pos, vel, accel = c.evaluate_at(c1.time - c1.time)
+        self.assertAlmostEqual(pos, c1.position, places=4)
+        self.assertAlmostEqual(vel, c1.velocity, places=4)
+        self.assertAlmostEqual(accel, c1.acceleration, places=4)
+
+        pos, vel, accel = c.evaluate_at(c2.time - c1.time)
+        self.assertAlmostEqual(pos, c2.position, places=4)
+        self.assertAlmostEqual(vel, c2.velocity, places=4)
+        self.assertAlmostEqual(accel, c2.acceleration, places=4)
+
+        pos, vel, accel = c.evaluate_at(4.584 - c1.time)
+        self.assertAlmostEqual(pos, 57.915, places=3)
+        self.assertNotAlmostEqual(vel, 0.0, places=4)
+        self.assertNotAlmostEqual(accel, 0, places=4)
