40ac84ed711d76242406a4d37d97730381314384
Binit Shah <bshah@hello-robot.com>
2021-08-16 17:48:00 -0700

    Refactor start/stop methods into init

--- body/stretch_body/transport/__init__.py
+++ body/stretch_body/transport/__init__.py
@@ -1,1 +1,33 @@
-import transport.rpc
+from __future__ import print_function
+import stretch_body.transport.rpc
+import serial
+import fcntl
+import errno
+import logging
+
+bus = None
+logger = logging.getLogger('transport')
+
+
+def start(usb):
+    global bus, logger
+    logger.debug('Opening transport on: {0}'.format(usb))
+    try:
+        bus = serial.Serial(usb, write_timeout=1.0)
+        fcntl.flock(bus.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except (serial.SerialException, IOError) as e:
+        if e.errno == errno.ENOENT:
+            logger.error('Device {0} does not exist. Check cable connections.'.format(usb))
+        elif e.errno == errno.EAGAIN:
+            logger.error('Device {0} busy. Close other Stretch Body instances.'.format(usb))
+            bus.close()
+        bus = None
+    return bus is not None
+
+
+def stop():
+    global bus, logger
+    if bus is not None:
+        logger.debug('Closing transport on: {0}'.format(bus.port))
+        bus.close()
+        bus = None
--- body/stretch_body/transport/rpc.py
+++ body/stretch_body/transport/rpc.py
@@ -1,10 +1,8 @@
 from __future__ import print_function
-from transport import encoder
+import stretch_body.transport as transport
+from stretch_body.transport import encoder
 import time
 import array
-import fcntl
-import serial
-import logging
 
 # methods
 RPC_START_NEW_RPC =100
@@ -23,56 +21,27 @@ RPC_DATA_SIZE = 1024
 READ_TIMEOUT = 0.2 # seconds
 PACKET_MARKER = 0
 
-bus = None
-logger = logging.getLogger()
-
-
-def start(usb):
-    global bus, logger
-    logger.warn('Starting transport on: ' + usb)
-    try:
-        bus = serial.Serial(usb, write_timeout=1.0)
-        if bus.isOpen():
-            try:
-                fcntl.flock(bus.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
-            except IOError:
-                logger.error('Port %s is busy. Check if another Stretch Body process is already running' % usb)
-                bus.close()
-                bus = None
-    except serial.SerialException as e:
-        logger.error("SerialException({0}): {1}".format(e.errno, e.strerror))
-        bus = None
-
-
-def stop():
-    global bus, logger
-    if bus is not None:
-        logger.warn('Shutting down transport')
-        bus.close()
-        bus = None
-
 
 def send(method, arguments_format, returns_format, arguments):
-    global bus, logger
-    if bus is None:
-        return
+    if transport.bus is None:
+        return False
     buffer = array.array('B', [0] * (RPC_BLOCK_SIZE*2))
     buffer[0] = RPC_START_NEW_RPC
     encoded_buffer = encoder.cobs_frame(data=buffer, size=1)
-    bus.write(encoded_buffer)
+    transport.bus.write(encoded_buffer)
     buffer, size = _receive_data()
     if buffer[0] != RPC_ACK_NEW_RPC:
-        logger.error('Transport RX Error on RPC_ACK_NEW_RPC')
-        return
+        transport.logger.error('Transport RX Error on RPC_ACK_NEW_RPC')
+        return False
+    return True
 
 
 def _receive_data():
-    global bus, logger
     start = time.time()
     rx_buffer = []
     while (time.time() - start) < READ_TIMEOUT:
-        nn = bus.inWaiting()
-        waiting_bytes = bus.read(nn) if nn > 0 else []
+        nn = transport.bus.inWaiting()
+        waiting_bytes = transport.bus.read(nn) if nn > 0 else []
         for byte in waiting_bytes:
             if type(byte) == str: # Py2 needs this, Py3 not
                 byte = struct.unpack('B', byte)[0]
