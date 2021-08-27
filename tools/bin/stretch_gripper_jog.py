7bf8f81f140f0a128a03e590dd67efc986f2ecf6
aedsinger <aedsinger@hello-robot.com>
2021-08-27 11:52:24 -0700

    added unittests for tools and fixed broken tools

--- tools/bin/stretch_gripper_jog.py
+++ tools/bin/stretch_gripper_jog.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import stretch_body.robot_params
 stretch_body.robot_params.RobotParams.set_logging_level("DEBUG")
-from __future__ import print_function
 import sys
 import stretch_body.stretch_gripper as gripper
 import argparse
