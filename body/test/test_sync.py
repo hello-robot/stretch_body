3c8e6b9052a95d7a164190717e75faf3c9067da5
hello-binit <64861565+hello-binit@users.noreply.github.com>
2021-09-23 20:28:53 -0700

    add motor sync unit test

--- body/test/test_sync.py
+++ body/test/test_sync.py
@@ -11,6 +11,48 @@ import time
 
 class TestSync(unittest.TestCase):
 
+    def test_motor_sync(self):
+        """Verify that the motor sync enable/disable works as expected
+        """
+        p = stretch_body.pimu.Pimu()
+        p.startup()
+        a = stretch_body.arm.Arm()
+        a.startup()
+
+        #First verify can move w/o sync_mode
+        a.motor.disable_sync_mode()
+        a.move_to(0.1)
+        a.push_command()
+        a.motor.wait_until_at_setpoint(timeout=5.0)
+        a.pull_status()
+        self.assertAlmostEqual(a.status['pos'], 0.1, places=1)
+
+        #Now verify in sync mode it doesn't move until the Pimu triggers
+        a.motor.enable_sync_mode()
+        a.push_command()
+        a.move_to(0.05)
+        a.push_command()
+        a.motor.wait_until_at_setpoint(timeout=2.0)
+        a.pull_status()
+        self.assertAlmostEqual(a.status['pos'], 0.1, places=1)
+
+        p.trigger_motor_sync()
+        time.sleep(2.0)
+        a.motor.wait_until_at_setpoint(timeout=2.0)
+        a.pull_status()
+        self.assertAlmostEqual(a.status['pos'], 0.05, places=1)
+
+        #Now turn sync mode off and verify that moves still
+        a.motor.disable_sync_mode()
+        a.move_to(0.15)
+        a.push_command()
+        a.motor.wait_until_at_setpoint(timeout=2.0)
+        a.pull_status()
+        self.assertAlmostEqual(a.status['pos'], 0.15, places=1)
+
+        p.stop()
+        a.stop()
+
     def test_sync_stream(self):
         """Check that streaming sync pulses to
         Stepper doesnt place it in runstop
@@ -31,4 +73,3 @@ class TestSync(unittest.TestCase):
             #time.sleep(0.01)
         self.assertAlmostEqual(r.arm.status['pos'], 0.3, places=1)
         r.stop()
-
