fe567d291f03cecc87ae41b6b258393538164c75
Blaine Matulevich <bm@hello-robot.com>
2021-12-15 05:22:37 -0800

    Include py3 in stretch_version

--- tools/bin/stretch_version.sh
+++ tools/bin/stretch_version.sh
@@ -1,58 +1,72 @@
 #!/bin/bash
 
-echo "# -----------------------------------------------" 
-echo "# VERSION INFORMATION FOR THE STRETCH RE1 FROM HELLO ROBOT" 
-echo "# -----------------------------------------------" 
-echo 
-echo "# stretch_about_text.py" 
+echo "# -----------------------------------------------"
+echo "# VERSION INFORMATION FOR THE STRETCH RE1 FROM HELLO ROBOT"
+echo "# -----------------------------------------------"
+echo
+echo "# stretch_about_text.py"
 stretch_about_text.py
-echo 
-echo "# -----------------------------------------------" 
-echo 
-echo "# uname -a" 
-uname -a 
-echo 
-echo "# -----------------------------------------------" 
-echo 
-echo "# rs-fw-update -l" 
-rs-fw-update -l 
-echo 
-echo "# -----------------------------------------------" 
-echo 
-echo "# pip list | grep hello" 
-pip list | grep hello 
 echo
-echo "# -----------------------------------------------" 
-echo 
-echo "# cd $HOME/catkin_ws/src/stretch_ros" 
-echo "# git log -1 " 
+echo "# -----------------------------------------------"
+echo
+echo "# uname -a"
+uname -a
+echo
+echo "# -----------------------------------------------"
+echo
+echo "# rs-fw-update -l"
+rs-fw-update -l
+echo
+echo "# -----------------------------------------------"
+echo
+echo "# cd $HOME/catkin_ws/src/stretch_ros"
+echo "# git log -1 "
 cd $HOME/catkin_ws/src/stretch_ros
-git log -1 
-echo 
-echo "# -----------------------------------------------" 
-echo 
-echo "# pip list | grep realsense" 
-pip list | grep realsense 
-echo 
-echo "# -----------------------------------------------" 
-echo 
-echo "# pip list | grep opencv" 
-pip list | grep opencv 
-echo 
-echo "# -----------------------------------------------" 
-echo 
-echo "# apt list --installed | grep realsense" 
-apt list --installed | grep realsense 
-echo 
-echo "# -----------------------------------------------" 
-echo 
-echo "# apt list --installed | grep opencv" 
-apt list --installed | grep opencv 
-echo 
-echo "# -----------------------------------------------" 
+git log -1
 echo
-echo "# FOR FUTURE IMPLEMENTATION"
+echo "# -----------------------------------------------"
+echo
+echo "# python -m pip list | grep hello"
+python -m pip list | grep hello
+echo
+echo "# -----------------------------------------------"
+echo
+echo "# python -m pip list | grep realsense"
+python -m pip list | grep realsense
+echo
+echo "# -----------------------------------------------"
+echo
+echo "# python -m pip list | grep opencv"
+python -m pip list | grep opencv
+echo
+echo "# -----------------------------------------------"
+echo
+echo "# python3 -m pip list | grep hello"
+python3 -m pip list | grep hello
+echo
+echo "# -----------------------------------------------"
+echo
+echo "# python3 -m pip list | grep realsense"
+python3 -m pip list | grep realsense
+echo
+echo "# -----------------------------------------------"
+echo
+echo "# python3 -m pip list | grep opencv"
+python3 -m pip list | grep opencv
+echo
+echo "# -----------------------------------------------"
+echo
+echo "# apt list --installed | grep realsense"
+apt list --installed | grep realsense
+echo
+echo "# -----------------------------------------------"
+echo
+echo "# apt list --installed | grep opencv"
+apt list --installed | grep opencv
+echo
+echo "# -----------------------------------------------"
+echo
 echo "# RE1_firmware_updater.py --current"
-# RE1_firmware_updater.py --current
-echo 
-echo "# -----------------------------------------------" 
+RE1_firmware_updater.py --current
+echo
+echo "# -----------------------------------------------"
