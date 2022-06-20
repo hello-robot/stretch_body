1adff1577c1becc315699d07076282c55e34d5f3
Aaron Edsinger <aedsinger@hello-robot.com>
2022-06-20 11:32:07 -0700

    fix dependency

--- body/setup.py
+++ body/setup.py
@@ -29,6 +29,6 @@ setuptools.setup(
                       'dynamixel-sdk>=3.1; python_version >= "3.0.0"', # py2 gets dynamixel-sdk through ROS
                       'pyyaml>=5.1', # required for yaml.FullLoader
                       'hello-robot-stretch-tool-share>=0.2.0', # defines other Stretch end effectors
-                      'hello-robot-stretch-factory>=0.3.0'
+                      'hello-robot-stretch-factory>=0.2.0'
                       ]
 )
