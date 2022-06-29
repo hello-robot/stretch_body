0a3d0b101a5f310ca34060aa3b7f4ecefcdb2742
Aaron Edsinger <aedsinger@hello-robot.com>
2022-06-29 08:34:55 -0700

    update to py3 for deploy

--- body/deploy.sh
+++ body/deploy.sh
@@ -3,7 +3,7 @@
 rm -rf dist
 rm -rf build
 rm -rf *.egg-info
-python setup.py sdist bdist_wheel
-python -m twine upload dist/*
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload dist/*
 #to install: pip2 install  hello-robot-stretch-body
 #to uninstall: pip2 uninstall hello-robot-stretch-body
