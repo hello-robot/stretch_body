ec9b774c5f862ab13d7f57c6c9679c81868c3a35
Mohamed Fazil <mfazil@hello-robot.com>
2023-08-02 12:47:20 -0700

    Fix dxl max vel print

--- body/test/test_dxl_vel.py
+++ body/test/test_dxl_vel.py
@@ -29,7 +29,7 @@ class TestDynamixelVelocity(unittest.TestCase):
     
     def velocity_control_test(self, set_vel_method):
         max_vel = self.dynamixel.motor.get_vel_limit()
-        print(f"Vel Limit: {max_vel} ticks/s | {degrees(self.dynamixel.ticks_to_rad(max_vel))} deg/s")
+        print(f"Vel Limit: {max_vel} ticks/s | {abs(self.dynamixel.ticks_to_world_rad_per_sec(max_vel))} rad/s")
         print(f"Vel gains P: {self.dynamixel.motor.get_vel_P_gain()} | I: {self.dynamixel.motor.get_vel_I_gain()}")
         
         # rotate X deg in Ts with constant Vel i.e. X == Vel*total_time
