95a7f294485bb822159f215f36209a4d9356e988
Mohamed Fazil <mfazil@hello-robot.com>
2023-08-10 17:48:52 -0700

    reorganize test tool

--- body/test/test_dxl_sine_vel.py
+++ body/test/test_dxl_sine_vel.py
@@ -7,8 +7,16 @@ from stretch_body import dynamixel_hello_XL430
 from stretch_body import robot
 import time
 import matplotlib.pyplot as plt
-import drawnow
-####### This is a experimental code and not an official unittest test code ######
+
+####### This is a an experimental test tool and not an unittest test code ######
+
+"""
+This script visualizes the safety braking mechanism of the dynamixel joints (head_pan, head_tilt, wrist_yaw) that tests the following:
+- dampened velocities are issued beyond the velocity braking zone
+- zero velocity maintained at the joint limits
+- Max velocities input near joint limits are braked smoothly and won't get into overoad error state.
+This test script sends an sine wave of input velocties at 0.1 Hz and amplitude equal to the maximum velocity. 
+"""
 
 class MultiDataMultiFramePlot:
     def __init__(self, y_data, title="", y_label=""):
@@ -42,63 +50,87 @@ class MultiDataMultiFramePlot:
 
         plt.tight_layout()
         plt.show()
-        
+
 def generate_sine_wave(amplitude, frequency, time, step, phase=0):
     time = np.arange(0, time, step)
     y_values = amplitude * np.sin((2 * np.pi * frequency * time)+ phase)
-
     return time, y_values
 
-r = robot.Robot()
-r.startup()
-# r.home()
+def plot_data(motor, time_values, y_values, vel_track, pos_track, effort_track):
+    joint_name = motor.name
+    min_pos = motor.get_soft_motion_limits()[0]
+    max_pos = motor.get_soft_motion_limits()[1]
+    plotter = MultiDataMultiFramePlot(y_data=time_values, y_label="Time",title=f"Velocity Control: {joint_name}")
+    plotter.add_data(y_values,"Target velocity [rad/s]")
+    plotter.add_data(vel_track,"velocity [rad/s]")
+    plotter.add_data(pos_track,"position [rad]",[(min_pos,'red'),
+                                                (max_pos,'red'),
+                                                (min_pos+motor.vel_brake_zone_thresh,'blue'), 
+                                                (max_pos-motor.vel_brake_zone_thresh,'blue')])
+    plotter.add_data(effort_track, "Effort")
+    plotter.plot()
+
+def run_test_on_motor(motor):
+    motor.home()
+    total_time = 30
+    interval = 1/30 # s
+    freaquency = 0.1 #Hz
+    phase = 0
+
+    max_vel_ticks = motor.motor.get_vel_limit()
+    print(f"Vel Limit: {max_vel_ticks} ticks/s | {abs(motor.ticks_to_world_rad_per_sec(max_vel_ticks))} rad/s")
+    print(f"Vel gains P: {motor.motor.get_vel_P_gain()} | I: {motor.motor.get_vel_I_gain()}")
+    max_vel = abs(motor.ticks_to_world_rad_per_sec(max_vel_ticks))
+
+    T, y_values = generate_sine_wave(max_vel,freaquency, total_time, interval, phase)
+    vel_track = []
+    pos_track = []
+    effort_track = []
+
+    start = time.time()
+    for x in y_values:
+        motor.set_velocity(x)
+        vel_track.append(motor.status['vel'])
+        pos_track.append(motor.status['pos'])
+        effort_track.append(motor.status['effort'])
+        time.sleep(interval)
+    elapsed = time.time() - start
+    print(f"Time Elapsed: {elapsed} s")
+    plot_data(motor, T, y_values, vel_track, pos_track, effort_track)
+    return T, y_values, vel_track, pos_track, effort_track
 
 
-# joint_name = 'head_pan'
-# motor = r.head.get_joint(joint_name)
-joint_name = 'wrist_yaw' # wrist_yaw, stretch_gripper, wrist_pitch, wrist_roll
-motor = r.end_of_arm.get_joint(joint_name)
-# motor = dynamixel_hello_XL430.DynamixelHelloXL430("wrist_yaw")
-# motor.startup()
-motor.home()
-total_time = 30
-interval = 1/30 # s
-freaquency = 0.1 #Hz
 
-phase = 0
-max_vel_ticks = motor.motor.get_vel_limit()
-print(f"Vel Limit: {max_vel_ticks} ticks/s | {abs(motor.ticks_to_world_rad_per_sec(max_vel_ticks))} rad/s")
-print(f"Vel gains P: {motor.motor.get_vel_P_gain()} | I: {motor.motor.get_vel_I_gain()}")
-max_vel = abs(motor.ticks_to_world_rad_per_sec(max_vel_ticks))
-# max_vel = 3
-min_pos = motor.get_soft_motion_limits()[0]
-max_pos = motor.get_soft_motion_limits()[1]
-# motor.set_vel_dead_zone(0.1)
-# deadzone = motor.vel_dead_zone
+def test_head_pan_joint():
+    r = robot.Robot()
+    r.startup()
 
-T, y_values = generate_sine_wave(max_vel,freaquency, total_time, interval, phase)
-vel_track = []
-pos_track = []
-effort_track = []
+    joint_name = 'head_pan'
+    motor = r.head.get_joint(joint_name)
+    T, input_velocities, vel_track, pos_track, effort_track = run_test_on_motor(motor)
+    r.stop()
 
-start = time.time()
-for x in y_values:
-    motor.set_velocity(x)
-    vel_track.append(motor.status['vel'])
-    pos_track.append(motor.status['pos'])
-    effort_track.append(motor.status['effort'])
-    time.sleep(interval)
-r.stop()
-elapsed = time.time() - start
-print(f"Time Elapsed: {elapsed} s")
 
-plotter = MultiDataMultiFramePlot(y_data=T,y_label="Time",title=f"Velocity Control: {joint_name}")
-plotter.add_data(y_values,"Target velocity [rad/s]")
-plotter.add_data(vel_track,"velocity [rad/s]")
-plotter.add_data(pos_track,"position [rad]",[(min_pos,'red'),
-                                             (max_pos,'red'),
-                                             (min_pos+motor.vel_brake_zone_thresh,'blue'), 
-                                             (max_pos-motor.vel_brake_zone_thresh,'blue')])
-plotter.add_data(effort_track, "Effort")
-plotter.plot()
 
+def test_head_tilt_joint():
+    r = robot.Robot()
+    r.startup()
+
+    joint_name = 'head_tilt'
+    motor = r.head.get_joint(joint_name)
+    T, input_velocities, vel_track, pos_track, effort_track = run_test_on_motor(motor)
+    r.stop()
+
+def test_wrist_yaw_joint():
+    r = robot.Robot()
+    r.startup()
+
+    joint_name = 'wrist_yaw'
+    motor = r.end_of_arm.get_joint(joint_name)
+    T, input_velocities, vel_track, pos_track, effort_track = run_test_on_motor(motor)
+    r.stop()
+
+if __name__=="__main__":
+    test_wrist_yaw_joint()
+    test_head_pan_joint()
+    test_head_tilt_joint()
