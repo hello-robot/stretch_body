b47dc58d89f1c4b3804e5a24475ec374581b89e1
Mohamed Fazil <mfazil@hello-robot.com>
2023-08-25 16:54:08 -0700

    wip

--- body/stretch_body/__init__.py
+++ body/stretch_body/__init__.py
@@ -2,5 +2,7 @@
 <img src="../../docs/images/banner.png" width="765">
 .. include:: ../README.md
 """
+# Control submodules to be presented
+# __all__ = ["arm", "base", "device", "dynamixel_hello_XL430", "lift"]
 from .version import __version__
 
--- docs/pdoc/make.py
+++ docs/pdoc/make.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     render.configure(logo="../images/banner.png",
                      logo_link='https://github.com/hello-robot/stretch_body',
                      template_directory="./custom-template/",
-                     favicon="../images/hello_robot_favicon.png")
+                     favicon="../images/hello_robot_favicon.png",
+                     edit_url_map="stretch_body=https://github.com/hello-robot/stretch_body/tree/feature/pdoc/body/stretch_body/")
 
     out = render.html_module(module=doc, all_modules={"stretch_body": doc})
 
