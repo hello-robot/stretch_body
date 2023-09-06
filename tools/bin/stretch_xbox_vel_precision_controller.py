de8b1181c71e3ba435a40a4432d18f41f7f5782a
Mohamed Fazil <mfazil@hello-robot.com>
2023-09-05 21:15:27 -0700

    precision mode for dxl

--- tools/bin/stretch_xbox_vel_precision_controller.py
+++ tools/bin/stretch_xbox_vel_precision_controller.py
@@ -317,14 +317,21 @@ class CommandArm:
         #       f"xv: {xv}\n"
         #       f"x_des: {x_des}\n"
         #       f"v_curr: {self.motor.status['vel']}\n")
+
+class CommandDxlJoint:
+    def __init__(self, robot, name):
+        if 'wrist' in name:
+            self.motor = robot.end_of_arm.get_joint(name)
+        if 'head' in name:
+            self.motor = robot.head.get_joint(name)
+        self.dead_zone = 0.001
+        self._prev_set_vel_ts = None
+        self.max_vel = self.motor.params['motion']['default']['vel']
+        self.safety_v = 0
+        self.scale_factor = 1
+        self.precision_mode = False
         
-class CommandWristYaw:
-    def __init__(self, robot):
-        self.motor = robot.end_of_arm.get_joint('wrist_yaw')
-        self._prev_set_vel_ts = None
-        self.max_linear_vel = self.motor.params['motion']['default']['vel']
-        self.safety_v = 0
-        self.scale_factor = 1
+        self.precision_scale_down = 0.05
     
     def _step_feedback_check(self,v):
         if self._prev_set_vel_ts is None:
@@ -338,118 +345,31 @@ class CommandWristYaw:
     def _process_stick_to_vel(self, x):
         # do someting
         x = -1*x
-        v = map_to_range(abs(x), 0, self.max_linear_vel)
+        v = map_to_range(abs(x), 0, self.max_vel)
         if x<0:
             v = -1*v
 
         self._step_feedback_check(v)
     
     def command_stick_to_motion(self, x):
+        if abs(x)<self.dead_zone:
+            x = 0
         x = to_parabola_transform(x)
         self._process_stick_to_vel(x)
+        if self.precision_mode:
+            self.safety_v = self.safety_v*self.precision_scale_down
         self.motor.set_velocity(self.safety_v)
         self._prev_set_vel_ts = time.time()
-        # print(f"[CommandWristYaw]  X: {x} || v: {self.safety_v}")
 
-class CommandWristPitch:
-    def __init__(self, robot):
-        self.motor = robot.end_of_arm.get_joint('wrist_pitch')
-        self._prev_set_vel_ts = None
-        self.max_linear_vel = self.motor.params['motion']['max']['vel']
-        self.safety_v = 0 
-        self.scale_factor = 1
-
-    def _step_feedback_check(self,v):
-        if self._prev_set_vel_ts is None:
-            return
-        if self.motor.status['timestamp_pc'] > self._prev_set_vel_ts:
-            # modify vel value based on 
-            # feedback from reading the status
-            pass
-        self.safety_v = v*self.scale_factor
-
-    def _process_stick_to_vel(self, x):
-        # do someting
-        x = -1*x
-        v = map_to_range(abs(x), 0, self.max_linear_vel)
-        if x<0:
-            v = -1*v
-
-        self._step_feedback_check(v)
-    
-    def command_stick_to_motion(self, x):
-        x = to_parabola_transform(x)
-        self._process_stick_to_vel(x)
-        self.motor.set_velocity(self.safety_v)
-        self._prev_set_vel_ts = time.time()
-        # print(f"[CommandWristPitch]  X: {x} || v: {self.safety_v}")
-
-class CommandHeadPan:
-    def __init__(self, robot):
-        self.motor = robot.head.get_joint('head_pan')
-        self._prev_set_vel_ts = None
-        self.max_linear_vel = self.motor.params['motion']['default']['vel']
-        self.safety_v = 0
-        self.scale_factor = 1
-    
-    def _step_feedback_check(self,v):
-        if self._prev_set_vel_ts is None:
-            return
-        if self.motor.status['timestamp_pc'] > self._prev_set_vel_ts:
-            # modify vel value based on 
-            # feedback from reading the status
-            pass
-        self.safety_v = v*self.scale_factor
-
-    def _process_stick_to_vel(self, x):
-        # do someting
-        x = -1*x
-        v = map_to_range(abs(x), 0, self.max_linear_vel)
-        if x<0:
-            v = -1*v
-
-        self._step_feedback_check(v)
-    
-    def command_stick_to_motion(self, x):
-        x = to_parabola_transform(x)
-        self._process_stick_to_vel(x)
-        self.motor.set_velocity(self.safety_v)
-        self._prev_set_vel_ts = time.time()
-        # print(f"[CommandHeadPan]  X: {x} || v: {self.safety_v}")
-
-class CommandHeadTilt:
-    def __init__(self, robot):
-        self.motor = robot.head.get_joint('head_tilt')
-        self._prev_set_vel_ts = None
-        self.max_linear_vel = self.motor.params['motion']['default']['vel']
-        self.safety_v = 0
-        self.scale_factor = 1
-    
-    def _step_feedback_check(self,v):
-        if self._prev_set_vel_ts is None:
-            return
-        if self.motor.status['timestamp_pc'] > self._prev_set_vel_ts:
-            # modify vel value based on 
-            # feedback from reading the status
-            pass
-        self.safety_v = v*self.scale_factor
-
-    def _process_stick_to_vel(self, x):
-        # do someting
-        x = -1*x
-        v = map_to_range(abs(x), 0, self.max_linear_vel)
-        if x<0:
-            v = -1*v
-
-        self._step_feedback_check(v)
-    
-    def command_stick_to_motion(self, x):
-        x = to_parabola_transform(x)
-        self._process_stick_to_vel(x)
-        self.motor.set_velocity(self.safety_v)
-        self._prev_set_vel_ts = time.time()
-        # print(f"[CommandHeadTilt]  X: {x} || v: {self.safety_v}")
-
+    def command_button_to_motion(self,direction):
+        vel = self.max_vel
+        if self.precision_mode:
+            vel = vel*self.precision_scale_down
+        if direction==1:
+            self.motor.set_velocity(vel)
+        elif direction==-1:
+            self.motor.set_velocity(-1*vel)
+            
 class CommandGripperPosition:
     def __init__(self, robot):
         self.motor = robot.end_of_arm.get_joint('stretch_gripper')
@@ -476,18 +396,19 @@ class TeleopController:
         self.base_command = CommandBase(self.robot)
         self.lift_command = CommandLift(self.robot)
         self.arm_command = CommandArm(self.robot)
-        self.wirst_yaw_command = CommandWristYaw(self.robot)
-        self.head_pan_command = CommandHeadPan(self.robot)
-        self.head_tilt_command =  CommandHeadTilt(self.robot)
+        self.wirst_yaw_command = CommandDxlJoint(self.robot,'wrist_yaw')
+        self.head_pan_command = CommandDxlJoint(self.robot,'head_pan')
+        self.head_tilt_command =  CommandDxlJoint(self.robot,'head_tilt')
         
         self.gripper = None
         self.wrist_pitch_command = None
         self.wrist_roll_command = None
         
-        if not self.end_of_arm_tool == 'tool_stretch_gripper':
+        if not self.end_of_arm_tool == 'tool_stretch_gripper' and not self.end_of_arm_tool=='tool_none':
             self.gripper = CommandGripperPosition(self.robot)
         if self.end_of_arm_tool == 'tool_stretch_dex_wrist':
-            self.wrist_pitch_command = CommandWristPitch(self.robot)
+            self.wrist_pitch_command = CommandDxlJoint(self.robot,'wrist_pitch')
+            self.wrist_roll_command = CommandDxlJoint(self.robot,'wrist_roll')
             
         print(f"Key mapped to End-Of-Arm Tool: {self.end_of_arm_tool}")
         
@@ -507,8 +428,58 @@ class TeleopController:
         self.robot.pimu.trigger_beep()
         self.robot.push_command()
         time.sleep(0.5)
-    
-    def command_joints(self):
+
+    def command_joints_A(self):
+        # Standard Key Mapping
+        if self.controller_state['right_shoulder_button_pressed']:
+            self.wirst_yaw_command.command_button_to_motion(-1)
+            
+        elif self.controller_state['left_shoulder_button_pressed']:
+            self.wirst_yaw_command.command_button_to_motion(1)
+        else:
+            self.wirst_yaw_command.command_stick_to_motion(0)
+        
+        if self.end_of_arm_tool == 'tool_stretch_dex_wrist':
+            if self.controller_state['top_pad_pressed']:
+                self.wrist_pitch_command.command_button_to_motion(1)
+            elif self.controller_state['bottom_pad_pressed']:
+                self.wrist_pitch_command.command_button_to_motion(-1)
+            else:
+                self.wrist_pitch_command.command_stick_to_motion(0)
+            
+            if self.controller_state['left_pad_pressed']:
+                self.wrist_roll_command.command_button_to_motion(1)
+            elif self.controller_state['right_pad_pressed']:
+                self.wrist_roll_command.command_button_to_motion(-1)
+            else:
+                self.wrist_roll_command.command_stick_to_motion(0)
+        else:
+            if self.controller_state['top_pad_pressed']:
+                self.head_tilt_command.command_button_to_motion(1)
+            elif self.controller_state['bottom_pad_pressed']:
+                self.head_tilt_command.command_button_to_motion(-1)
+            else:
+                self.head_tilt_command.command_stick_to_motion(0)
+            
+            if self.controller_state['left_pad_pressed']:
+                self.head_pan_command.command_button_to_motion(1)
+            elif self.controller_state['right_pad_pressed']:
+                self.head_pan_command.command_stick_to_motion(-1)
+            else:
+                self.head_pan_command.command_stick_to_motion(0)
+                
+        self.arm_command.command_stick_to_motion(self.controller_state['right_stick_x'])
+        self.lift_command.command_stick_to_motion(self.controller_state['right_stick_y'])
+        self.base_command.command_stick_to_motion(self.controller_state['left_stick_x'],self.controller_state['left_stick_y'])
+
+        if self.gripper:
+            if self.controller_state['right_button_pressed']:
+                self.gripper.open_gripper()
+            elif self.controller_state['bottom_button_pressed']:
+                self.gripper.close_gripper()
+
+    def command_joints_B(self):
+        # All analog key mapping
         if self.controller_state['right_shoulder_button_pressed']:
             self.wirst_yaw_command.command_stick_to_motion(self.controller_state['right_stick_x'])
             if self.wrist_pitch_command:
@@ -544,7 +515,16 @@ class TeleopController:
                 self.arm_command.precision_mode = self.precision_mode
                 self.lift_command.precision_mode = self.precision_mode
                 self.base_command.precision_mode = self.precision_mode
-                self.command_joints()
+                self.wirst_yaw_command.precision_mode = self.precision_mode
+                if self.gripper:
+                    self.gripper.precision_mode = self.precision_mode
+                if self.end_of_arm_tool == 'tool_stretch_dex_wrist':
+                    self.wrist_pitch_command.precision_mode = self.precision_mode
+                    self.wrist_roll_command.precision_mode = self.precision_mode
+                self.head_pan_command.precision_mode = self.precision_mode
+                self.head_tilt_command.precision_mode = self.precision_mode
+                self.command_joints_A()
+                # self.command_joints_B()
                 self.robot.push_command()
         else:
             print('press the start button to calibrate the robot')
@@ -561,8 +541,6 @@ class TeleopController:
         except (ThreadServiceExit, KeyboardInterrupt, SystemExit, UnpluggedError):
             self.xbox_controller.stop()
             self.robot.stop()
-
-
 
 if __name__ == "__main__":
    controller = TeleopController()
