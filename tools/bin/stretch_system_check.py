c3a9f8be540b1348dfa42a2f8faa198579410f74
Binit Shah <bshah@hello-robot.com>
2024-01-26 09:34:44 -0800

    Fix stepper_type check

--- tools/bin/stretch_system_check.py
+++ tools/bin/stretch_system_check.py
@@ -120,16 +120,16 @@ def are_actuators_ready():
         return False, "robot not homed"
 
     # Check hello steppers' self recognized type matches SDK's expectation
-    if 'stepper_type' in r.lift.motor.board_info:
+    if 'stepper_type' in r.lift.motor.board_info and r.lift.motor.board_info['stepper_type'] != 0:
         if r.lift.motor.board_info['stepper_type'] != 'hello-motor-lift':
             return False, "stepper type mismatch on lift motor"
-    if 'stepper_type' in r.arm.motor.board_info:
+    if 'stepper_type' in r.arm.motor.board_info and r.arm.motor.board_info['stepper_type'] != 0:
         if r.arm.motor.board_info['stepper_type'] != 'hello-motor-arm':
             return False, "stepper type mismatch on arm motor"
-    if 'stepper_type' in r.base.left_wheel.board_info:
+    if 'stepper_type' in r.base.left_wheel.board_info and r.base.left_wheel.board_info['stepper_type'] != 0:
         if r.base.left_wheel.board_info['stepper_type'] != 'hello-motor-left-wheel':
             return False, "stepper type mismatch on left wheel motor"
-    if 'stepper_type' in r.base.right_wheel.board_info:
+    if 'stepper_type' in r.base.right_wheel.board_info and r.base.right_wheel.board_info['stepper_type'] != 0:
         if r.base.right_wheel.board_info['stepper_type'] != 'hello-motor-right-wheel':
             return False, "stepper type mismatch on right wheel motor"
 
