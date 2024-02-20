8a5938f2f807fd411624c086690cec5ff6d04017
Binit Shah <bshah@hello-robot.com>
2024-02-20 02:00:54 -0500

    implement determine_what_tool_is_correct()

--- tools/bin/stretch_configure_tool.py
+++ tools/bin/stretch_configure_tool.py
@@ -6,6 +6,7 @@ stretch_body.robot_params.RobotParams.set_logging_formatter("brief_console_forma
 import stretch_body.hello_utils as hu
 hu.print_stretch_re_use()
 
+import sys
 import logging
 import argparse
 import stretch_body.device
@@ -51,10 +52,12 @@ def is_d405_present():
 cli_device.logger.info('Loading...')
 robot_device = stretch_body.device.Device(name='robot')
 stretch_tool = robot_device.params.get('tool', '')
-supported_tools = robot_device.robot_params.get('supported_eoa', [])
+# supported_tools = robot_device.robot_params.get('supported_eoa', []) # TODO
+supported_tools = ['tool_none', 'tool_stretch_gripper', 'tool_stretch_dex_wrist', 'eoa_wrist_dw3_tool_nil', 'eoa_wrist_dw3_tool_sg3']
 # num_wrist_dxls = how_many_dxl_on_wrist_chain() # TODO
 num_wrist_dxls = 4
-d405_present = is_d405_present()
+# d405_present = is_d405_present() # TODO
+d405_present = True
 
 def does_tool_need_to_change():
     cli_device.logger.info(f'Your software is currently configured for the {Fore.CYAN + stretch_tool + Style.RESET_ALL} tool')
@@ -88,8 +91,51 @@ def does_tool_need_to_change():
     cli_device.logger.info("Not changing anything. Exiting.")
     return False
 
-does_tool_need_to_change()
-# print(stretch_tool)
-# print(supported_tools)
-# print(num_wrist_dxls)
-# print(d405_present)
+def determine_what_tool_is_correct():
+    cli_device.logger.info('Determining what tool is correct for your robot...')
+
+    # start with the supported list
+    matches = supported_tools
+    cli_device.logger.debug(f"Starting with the supported tools for your model: {Fore.CYAN + str(matches) + Style.RESET_ALL}")
+
+    # filter by num dxls
+    numdxls_tool_map = {
+        1: ['tool_none'],
+        2: ['tool_stretch_gripper'],
+        3: ['eoa_wrist_dw3_tool_nil'],
+        4: ['tool_stretch_dex_wrist', 'eoa_wrist_dw3_tool_sg3']
+    }
+    numdxls_match = numdxls_tool_map.get(num_wrist_dxls, [])
+    cli_device.logger.debug(f"These tools match based on {num_wrist_dxls} number of wrist dxls: {Fore.CYAN + str(numdxls_match) + Style.RESET_ALL}")
+    matches = list(set(matches) & set(numdxls_match))
+    cli_device.logger.debug(f"Filtering based on this brings the matches to: {Fore.CYAN + str(matches) + Style.RESET_ALL}")
+
+    # filter by d405 present
+    d405_tool_map = {
+        False: ['tool_none', 'tool_stretch_gripper', 'tool_stretch_dex_wrist'],
+        True: ['eoa_wrist_dw3_tool_nil', 'eoa_wrist_dw3_tool_sg3'],
+    }
+    d405_match = d405_tool_map.get(d405_present, [])
+    cli_device.logger.debug(f"These tools match based on present={d405_present} gripper camera: {Fore.CYAN + str(d405_match) + Style.RESET_ALL}")
+    matches = list(set(matches) & set(d405_match))
+    cli_device.logger.debug(f"Filtering based on this brings the matches to: {Fore.CYAN + str(matches) + Style.RESET_ALL}")
+
+    if len(matches) == 0:
+        cli_device.logger.info('Unable to find any tool that matches the hardware connected to your robot. Contact Hello Robot support for help.')
+        cli_device.logger.info("Not changing anything. Exiting.")
+        sys.exit(1)
+    elif len(matches) > 1:
+        cli_device.logger.info('Found multiple possible tools that could work:')
+        for i, match in enumerate(matches):
+            cli_device.logger.info(f'    {i}) {match}')
+        user_select = int(input('Select one. If unsure, Ctrl C to exit & contact Hello Robot support. Input #: '))
+        if user_select >= len(matches):
+            cli_device.logger.info('Invalid selection. Exiting...')
+            sys.exit(1)
+        return matches[user_select]
+    else:
+        return matches[0]
+
+if does_tool_need_to_change():
+    target_tool_name = determine_what_tool_is_correct()
+    print(target_tool_name)
