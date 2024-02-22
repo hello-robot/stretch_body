5a098172e682a836995d83980ba25284d4adfc4e
Binit Shah <bshah@hello-robot.com>
2024-02-22 13:55:59 -0500

    Add back robot_system_check with deprecation notice

---
+++ tools/bin/stretch_robot_system_check.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import stretch_body.hello_utils as hu
+hu.print_stretch_re_use()
+
+import sys
+
+print("This tool has been deprecated. Its replacement is: stretch_system_check.py")
+sys.exit(1)
