ebb81a5d8061d892cda4144b8240b493b54c00b1
Binit Shah <bshah@hello-robot.com>
2025-01-23 20:51:14 -0500

    Remove numba
    FUNMAP is going to use numba through a virtual env

--- body/setup.py
+++ body/setup.py
@@ -25,7 +25,6 @@ setuptools.setup(
                       'scikit-image', 'open3d', 'pyrealsense2', 'pathlib', 'psutil', 'gitpython', 'urchin', 'urdf_parser_py', # urdfpy ==> urchin
                       'opencv-contrib-python', 'renamed-opencv-python-inference-engine; python_version >= "3.0.0"', # resolve cv2 conflict for py3
                       'jupyter',
-                      'numba', # numba required by stretch_funmap
                       'transforms3d>=0.4.2', # required by stretch_core
                       'dynamixel-sdk',
                       'pyyaml>=5.1', # required for yaml.FullLoader
