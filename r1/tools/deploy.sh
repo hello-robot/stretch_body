c5eeaadf63f8652b37a5d7f14244355ade9cc567
Aaron Edsinger <aedsinger@hello-robot.com>
2025-02-17 15:37:45 -0800

    wip

--- r1/tools/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#build and upload to pYPi
-rm -rf dist
-rm -rf build
-rm -rf *.egg-info
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/*
-#to install: pip2 install  hello-robot-stretch-body-tools
-#to uninstall: pip2 uninstall hello-robot-stretch-body-tools
