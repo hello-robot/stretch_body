1196b9641bde6cc35d3c8ea478a0b6e0ceae9106
Mohamed Fazil <mfazil@hello-robot.com>
2025-07-29 16:37:42 -0700

    Update gripper jog tool

--- tools/bin/stretch_gripper_jog.py
+++ tools/bin/stretch_gripper_jog.py
@@ -8,10 +8,18 @@ import argparse
 import stretch_body.hello_utils as hu
 hu.print_stretch_re_use()
 
+
+config_params = stretch_body.robot_params.RobotParams().get_params()[1]
+py_class = config_params[config_params['robot']['tool']]['devices']['stretch_gripper']['py_class_name']
+parser=argparse.ArgumentParser(description='Calibrate the gripper position by closing until motion stops')
+args=parser.parse_args()
+GripperClass = getattr(gripper, py_class)
+
+
 parser=argparse.ArgumentParser(description='Jog the griper from the keyboard')
 args=parser.parse_args()
 
-g=gripper.StretchGripper()
+g=GripperClass()
 if not g.startup(threaded=False):
     exit()
 g.pull_status()
